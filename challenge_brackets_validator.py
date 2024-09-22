f3cf460ba7aadb0cbc41c16abb8a819e7ccb8ed3
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2024-09-22 15:45:42 -0300

    fixing-brackets-validator-challenge

--- challenge_brackets_validator.py
+++ challenge_brackets_validator.py
@@ -1,15 +1,14 @@
 if __name__ == "__main__":
     brackets_string = input("Brackets String: ")
+    brackets = ["()", "[]", "{}"]
     
     while True:
-        brackets = ["()", "[]", "{}"]
-        
         for bracket in brackets:
             brackets_string = brackets_string.replace(bracket, "")
         
         if len(brackets_string) == 0:
             print("valid")
             break
-        elif any(bracket not in brackets_string for bracket in brackets):
+        elif all(bracket not in brackets_string for bracket in brackets):
             print("invalid")
             break
