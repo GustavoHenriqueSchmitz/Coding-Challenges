dc7b7845445b7457a3c975d1e94ab2bea8954084
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2025-01-08 10:59:47 -0300

    fixing 'add two numbers' challenge'

--- challenge_add_two_numbers.py
+++ challenge_add_two_numbers.py
@@ -1,12 +1,17 @@
-def add_two_numbers(listNumber1: int, listNumber2: int) -> bool:
-    total_sum = int("".join(map(str, listNumber1))) + int("".join(map(str, listNumber2)))
+def add_two_numbers(listNumber1, listNumber2):
+    listNumber1.reverse()
+    listNumber2.reverse()
+    
+    number1 = "".join(map(str, listNumber1))
+    number2 = "".join(map(str, listNumber2))
+    
+    total_sum = int(number1) + int(number2)
     result = list(str(total_sum))
     result.reverse()
     return result
 
 if __name__ == "__main__":
-    numbers1 = [3, 2, 1]
-    numbers2 = [3, 2, 1]
-    
+    numbers1 = list(input("Number 1: "))
+    numbers2 = list(input("Number 2: "))
     result = add_two_numbers(numbers1, numbers2)
     print(result)
